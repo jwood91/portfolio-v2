7425c72f8b4357a753368299c1adfa8a103dbfba
Jwood91 <Jwood4589@gmail.com>
2023-07-13 00:37:11 +0100

    docker now working on the project and nuxt3 setup to run

--- portfolio-v2/app.vue
+++ portfolio-v2/app.vue
@@ -1,5 +1,17 @@
 <template>
+<div>
+  <div>
+    <h1>testing</h1>
+  </div>  
   <div>
     <NuxtWelcome />
   </div>
+</div>  
 </template>
+<script>
+</script>  
+<style lang="css">
+h1 {
+  color: blue;
+}
+</style>  
