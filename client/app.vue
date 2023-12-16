334e927b20a8466432cf27ea82285b4221aa631c
Jwood91 <Jwood4589@gmail.com>
2023-12-16 12:06:26 +0000

    Fixed docker container to run with hot reload

--- client/app.vue
+++ client/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>HEY</h1>
+    <h1 class="font-bold">HEY There Fella How are you doing</h1>
     <NuxtWelcome />
   </div>
 </template>
+<style>
+h1 {
+  color: blue;
+}
+</style>
